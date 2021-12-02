20ff2e28f79b36909f4d743a3c35454f17bef5db
Noppas1n009 <64309010009@chontech.ac.th>
2021-12-02 16:07:06 +0700

    test11

---
+++ day12/show_demo.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "it64309010009";
+$password = "it123456";
+$dbname = "it6430901009";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// $sql = "SELECT * FROM demo";
+// $result = mysqli_query($conn, $sql);
+
+$sql=mysqli_query($conn,"SELECT * FROM demo");
+
+if (mysqli_num_rows($sql) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "id: ".$row["id"]." - Name: ".$row["name"]." - Address: ".$row["address"]."<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
+?>
